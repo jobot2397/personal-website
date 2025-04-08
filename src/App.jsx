b746fb46fff85e2f86628ee25d97a6ea1e2808c5
JoeLawler23 <joe-lawler-23@protonmail.com>
2025-04-07 22:15:30 -0500

    Test page

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+    return (
+        <div className=" h-screen w-full bg-amber-500">
+            <h1>
+                Joseph Lawler
+            </h1>
+        </div>
+    );
+}
